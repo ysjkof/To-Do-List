e82e6c98e4ad5b0ba97b572b50ee07dcd4bbfbae
seongjin <ysjkof@hanmail.net>
2021-03-16 20:27:41 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-const selectorPendingForm = document.querySelector(".pendingForm"),
-  selectorPendingFormInput = selectorPendingForm.querySelector("input"),
-  selectorPendingTitle = document.querySelector(".pending"),
+const pendingForm = document.querySelector(".pendingForm"),
+  pendingFormInput = pendingForm.querySelector("input"),
+  pendingTitle = document.querySelector(".pending"),
   selectorPendingList = document.querySelector(".pending-list"),
-  selectorFinishedTitle = document.querySelector(".finished"),
-  selectorFinishedList = document.querySelector(".finished-list"),
-  result = document.querySelector(".result");
-
-const makeLi = document.createElement("li"),
-  makeSpan = document.createElement("span"),
-  delBtn = document.createElement("button"),
-  switchBtn = document.createElement("button");
+  finishedTitle = document.querySelector(".finished"),
+  selectorFinishedList = document.querySelector(".finished-list");
 
 function getRandomNumber() {
   return Math.random() * 100000000000000000;
@@ -18,94 +12,202 @@ function getRandomNumber() {
 let pendingList = [],
   finishedList = [];
 
-function renderPage() {
-  loadLocalStorage();
+function renderPage(currentValue, array) {
+  console.log("------------------렌더 페이지 시작");
+  const makeLi = document.createElement("li"),
+    makeSpan = document.createElement("span"),
+    makeDelBtn = document.createElement("button"),
+    makeSwitchBtn = document.createElement("button");
 
-  finishedList.forEach((value) => document.write(value.text));
-  // delBtn.innerHTML = "❌";
-  // switchBtn.innerHTML = "✅";
-  // makeSpan.innerText = currentValue;
-  // makeLi.id = pendingList.text
-  // makeLi.appendChild(makeSpan);
-  // makeLi.appendChild(delBtn);
-  // makeLi.appendChild(switchBtn);
-  // selectList.appendChild(makeLi);
-  // }
+  console.log(currentValue);
+  makeDelBtn.innerText = "❌";
+  makeDelBtn.classList.add("del-btn");
+  makeDelBtn.addEventListener("click", handleDelBtn);
+  makeSwitchBtn.innerText = "✅";
+  makeSwitchBtn.className = "switch-btn";
+  makeSwitchBtn.addEventListener("click", handleSwitchBtn);
+  makeSpan.innerText = currentValue.text;
+  makeLi.id = currentValue.id;
+  makeLi.appendChild(makeSpan);
+  makeLi.appendChild(makeDelBtn);
+  makeLi.appendChild(makeSwitchBtn);
+  if (array == finishedList) {
+    selectorFinishedList.appendChild(makeLi);
+  } else {
+    selectorPendingList.appendChild(makeLi);
+  }
+}
+
+function saveLocalStorage(array) {
+  console.log("----------save local storage를 시작합니다.");
+  function truePendingList() {
+    const savePending = localStorage.setItem(
+      "pending",
+      JSON.stringify(pendingList)
+    );
+    if (savePending === null) {
+      console.log("----------저장할 Pending 값이 없습니다. null");
+    } else {
+      "finished", console.log("----------저장합니다: 팬딩 데이터");
+      savePending;
+    }
+  }
+  function trueFinishedList() {
+    const saveFinished = localStorage.setItem(
+      "finished",
+      JSON.stringify(finishedList)
+    );
+    if (saveFinished === null) {
+      console.log("---------저장할 Finished 값이 없습니다. null");
+    } else {
+      console.log("-----------저장합니다: 피니시 데이터");
+      saveFinished;
+    }
+  }
+  if (array == pendingList) {
+    truePendingList();
+  } else if (array == finishedList) {
+    trueFinishedList();
+  }
 }
 
 function loadLocalStorage() {
-  const loadLocalStoragePending = localStorage.getItem(
+  console.log("----------------로드 시작");
+  console.log(pendingList);
+  console.log(finishedList);
+  const loadPending = localStorage.getItem(
     "pending",
     JSON.stringify(pendingList)
   );
-  const loadLocalStorageFinished = localStorage.getItem(
+  const loadFinished = localStorage.getItem(
     "finished",
     JSON.stringify(finishedList)
   );
-
-  const parsedPending = JSON.parse(loadLocalStoragePending);
-  const parsedFinished = JSON.parse(loadLocalStorageFinished);
-
-  pendingList.push(parsedPending);
-  finishedList.push(parsedFinished);
-  // console.log(pendingList);
-  // console.log(finishedList);
-}
-
-function saveLocalStorage(targetList) {
-  if (targetList == finishedList) {
-    localStorage.setItem("finished", JSON.stringify(finishedList));
+  if (loadPending === null) {
+    console.log("Pending 값이 없습니다. null");
   } else {
-    localStorage.setItem("pending", JSON.stringify(pendingList));
+    const parsedPending = JSON.parse(loadPending);
+    pendingList = parsedPending;
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(pendingList);
+    pendingList.forEach(function (list) {
+      renderPage(list);
+    });
+    console.log("--------Pending Render 끝-----");
+    console.log(parsedPending);
   }
+
+  if (loadFinished === null) {
+    console.log("Finished 값이 없습니다. null");
+  } else {
+    const parsedFinished = JSON.parse(loadFinished);
+    finishedList = parsedFinished;
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(finishedList);
+    finishedList.forEach(function (list) {
+      renderPage(list, finishedList);
+    });
+    console.log("--------Finishied Render 끝-----");
+    console.log(parsedFinished);
+  }
+  console.log("---------------- 로드 끝");
 }
 
 function makeObject(currentValue, array) {
   // console.log(currentValue);
   // console.log(array);
-  const loadLocalStoragePending = localStorage.getItem(
-    "pending",
-    JSON.stringify(pendingList)
-  );
-  const loadLocalStorageFinished = localStorage.getItem(
-    "finished",
-    JSON.stringify(finishedList)
-  );
-  const newId = getRandomNumber();
+  console.log("---------------make Object 시작.");
+  console.log(currentValue);
+  console.log(array);
   const newObj = {
-    id: newId,
+    id: getRandomNumber(),
     text: currentValue,
   };
+  console.log(newObj);
   array.push(newObj);
+  renderPage(newObj, array);
+  console.log(array);
   saveLocalStorage(array);
 }
 
 function handleSwitchBtn(event) {
-  event.preventDefault();
-  const currentValue = switchBtn.value();
+  console.log("------------SWITCH------------");
+  const btn = event.target,
+    li = btn.parentNode,
+    div = li.parentNode,
+    innerText = li.childNodes[0].innerText;
+  // console.dir(li.id);
+  // console.log(li.childNodes[0].innerText);
+  // console.log(pendingList);
+  // console.log(pendingList);
+
+  if (div == selectorPendingList) {
+    console.log("-------------switch: pending 클릭");
+    selectorPendingList.removeChild(li);
+    const newList = pendingList.filter(function (tomato) {
+      return tomato.id !== parseInt(li.id);
+    });
+    pendingList = newList;
+
+    saveLocalStorage(pendingList);
+    makeObject(innerText, finishedList);
+    // renderPage(innerText, pendingList);
+  } else {
+    console.log("-------------switch: finished 클릭");
+    selectorFinishedList.removeChild(li);
+    const newList = finishedList.filter(function (tomato) {
+      return tomato.id !== parseInt(li.id);
+    });
+    finishedList = newList;
+
+    saveLocalStorage(finishedList);
+    makeObject(innerText, pendingList);
+  }
+
+  // makeObject(currentValue, )
 }
 
 function handleDelBtn(event) {
-  event.preventDefault();
-  // console.log(event);
-  const currentValue = delBtn.value();
-  // console.log(currentValue);
-  // const btn = event.target;
-  // const li = btn.parentNode;
-  // const div = li.parentNode;
+  console.log("------------DELETE------------");
+  const btn = event.target,
+    li = btn.parentNode,
+    div = li.parentNode;
+  console.log("---------1");
+  console.log(li.id);
+  if (div == selectorPendingList) {
+    selectorPendingList.removeChild(li);
+    const newList = pendingList.filter(function (tomato) {
+      return tomato.id !== parseInt(li.id);
+    });
+    console.log("---------2");
+    console.log(newList);
+
+    pendingList = newList;
+    saveLocalStorage(pendingList);
+  } else {
+    const btn = event.target,
+      li = btn.parentNode;
+    selectorFinishedList.removeChild(li);
+    const newList = finishedList.filter(function (toDoo) {
+      return toDoo.id !== parseInt(li.id);
+    });
+    finishedList = newList;
+    saveLocalStorage(finishedList);
+  }
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = selectorPendingFormInput.value;
+  const currentValue = pendingFormInput.value;
   makeObject(currentValue, pendingList);
+  // loadLocalStorage();
+  pendingFormInput.value = "";
 }
 
 function init() {
-  selectorPendingForm.addEventListener("submit", handleSubmit);
-  delBtn.addEventListener("click", handleDelBtn);
-  switchBtn.addEventListener("click", handleSwitchBtn);
-  renderPage();
+  // 먼저 불러와서 저장해야됨.
+  loadLocalStorage();
+  pendingForm.addEventListener("submit", handleSubmit);
 }
 
 init();
